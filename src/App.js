07e7a5f053a851a4a05e124f43d897c9188abbb0
ShreyaSuddala <shreyasuddala27@gmail.com>
2024-04-08 12:11:25 +0530

    created profile section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { useEffect } from 'react';
 import './App.css';
 import Navbar from './layouts/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   useEffect(()=>{
     document.title ="Portfolio"
   },[])
   return (
-    <div >
+    < >
      <Navbar/>
-    </div>
+     <div className='container' >
+
+     <Profile/>
+     </div>
+    </>
   );
 }
 
